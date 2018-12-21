f2bdb8cdaeea30c06530e9d3d96e080cca204c53
Thanh Sơn Bành <takashato@gmail.com>
2018-12-21 15:17:52 +0700

    Copy tools from server to client

---
+++ vTalkClient/tools/PacketProcessor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vTalkServer.tools
+{
+    class PacketProcessor
+    {
+        /// <summary>
+        /// Packet's Header Size, include Packet Length (int32) and header (int8)
+        /// </summary>
+        public static int HeaderSize = 5;
+
+        /// <summary>
+        /// Waiting state
+        /// </summary>
+        private bool IsWaiting { get; set; }
+
+        /// <summary>
+        /// Data buffer
+        /// </summary>
+        private byte[] DataBuffer { get; set; }
+
+        /// <summary>
+        /// Amount of data to wait on
+        /// </summary>
+        private int WaitForData { get; set; }
+
+        /// <summary>
+        /// Callback for when a packet is finished
+        /// </summary>
+        public delegate void CallPacketFinished(byte[] packet);
+
+        /// <summary>
+        /// Event called when a packet has been handled
+        /// </summary>
+        public event CallPacketFinished PacketFinished;
+
+        /// <summary>
+        /// Create new instance of <see cref="PacketProcessor"/>
+        /// </summary>
+        public PacketProcessor()
+        {
+            DataBuffer = new byte[0x100];
+            WaitForData = 0;
+            IsWaiting = true;
+        }
+
+        /// <summary>
+        /// Add binary data to the buffer
+        /// </summary>
+        /// <param name="data">data buffer to add</param>
+        /// <param name="offset">data offset</param>
+        /// <param name="length">data length</param>
+        public void AddData(byte[] data, int offset, int length)
+        {
+            if (IsWaiting)
+            {
+                if(length < HeaderSize) // Packet error, not enough header byte
+                {
+                    return;
+                }
+
+                PacketReader pr = new PacketReader(data);
+                int packetLength = pr.ReadInt(); // Get packet length 
+                DataBuffer = new byte[packetLength + HeaderSize]; // Create new buffer with length = packetLength + HeaderSize
+                Buffer.BlockCopy(data, 0, DataBuffer, 0, length); // Copy data to general buffer
+                if (length < packetLength + HeaderSize) // If packet length including header size is smaller than actual size, wait for the remaining
+                {
+                    WaitForData = packetLength + HeaderSize - length;
+                    IsWaiting = false;
+                }
+                else // Finish processing packet
+                {
+                    PacketFinished?.Invoke(DataBuffer); // Invoke Finish handler
+                }
+            }
+            else // Remaining received
+            {
+                Buffer.BlockCopy(data, 0, DataBuffer, DataBuffer.Length - WaitForData, length); // Add data in this buffer to general buffer
+                WaitForData -= length; // Decrease data amount to be waited
+                if (WaitForData <= 0) // Received all data
+                {
+                    if (PacketFinished != null)
+                        PacketFinished(DataBuffer);
+                    IsWaiting = true; // Reset processor
+                }
+            }
+        }
+    }
+}
---
+++ vTalkClient/tools/PacketWriter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vTalkServer.tools
+{
+    public class PacketWriter
+    {
+
+        private byte[] Buffer { get; set; }
+        public int Length { get; private set; }
+        public int Position { get; set; }
+
+        public PacketWriter()
+        {
+            Buffer = new byte[0x50];
+        }
+
+        private void EnsureCapacity(int length)
+        {
+            if (Position + length < Buffer.Length) return;
+            byte[] newBuffer = new byte[Buffer.Length + 0x50];
+            System.Buffer.BlockCopy(Buffer, 0, newBuffer, 0, Buffer.Length);
+            Buffer = newBuffer;
+            EnsureCapacity(length);
+        }
+
+        public void WriteBytes(byte[] bytes)
+        {
+            int length = bytes.Length;
+            if (bytes == null || length == 0)
+                throw new ArgumentNullException("bytes", "Trying to write zero or null bytes");
+
+            EnsureCapacity(length);
+            System.Buffer.BlockCopy(bytes, 0, Buffer, Position, length);
+
+            Length += length;
+            Position += length;
+        }
+
+        public void WriteBool(bool value)
+        {
+            WriteByte((byte)(value ? 1 : 0));
+        }
+
+        public void WriteSByte(sbyte value)
+        {
+            sbyte[] signed = { value };
+            WriteBytes((byte[])(Array)signed);
+        }
+
+        public void WriteByte(byte value)
+        {
+            WriteBytes(new byte[1] { value });
+        }
+
+        public void WriteShort(short value)
+        {
+            WriteBytes(new byte[2] {
+                (byte)value,
+                (byte)(value >> 8)
+            });
+        }
+
+        public void WriteUShort(ushort value)
+        {
+            WriteBytes(new byte[2] {
+                (byte)value,
+                (byte)(value >> 8)
+            });
+        }
+
+        public void WriteInt(int value)
+        {
+            WriteBytes(new byte[4] {
+                (byte)value,
+                (byte)(value >> 8),
+                (byte)(value >> 16),
+                (byte)(value >> 24)
+            });
+        }
+
+        public void WriteUInt(uint value)
+        {
+            WriteBytes(new byte[4] {
+                (byte)value,
+                (byte)(value >> 8),
+                (byte)(value >> 16),
+                (byte)(value >> 24)
+            });
+        }
+
+        public void WriteLong(long value)
+        {
+            WriteBytes(new byte[8] {
+                (byte)value,
+                (byte)(value >> 8),
+                (byte)(value >> 16),
+                (byte)(value >> 24),
+                (byte)(value >> 32),
+                (byte)(value >> 40),
+                (byte)(value >> 48),
+                (byte)(value >> 56)
+            });
+        }
+
+        public void WriteULong(ulong value)
+        {
+            WriteBytes(new byte[8] {
+                (byte)value,
+                (byte)(value >> 8),
+                (byte)(value >> 16),
+                (byte)(value >> 24),
+                (byte)(value >> 32),
+                (byte)(value >> 40),
+                (byte)(value >> 48),
+                (byte)(value >> 56)
+            });
+        }
+
+        public void WriteString(string str)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            WriteInt(bytes.Length);
+            WriteBytes(bytes);
+        }
+
+        public byte[] ToArray()
+        {
+            byte[] toRet = new byte[Length];
+            System.Buffer.BlockCopy(Buffer, 0, toRet, 0, Length);
+            return toRet;
+        }
+    }
+}
