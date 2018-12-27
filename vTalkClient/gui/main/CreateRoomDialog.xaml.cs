40b126106657a39ea51e58e976dd5ce449850e3c
PTChinh <PTChinh@users.noreply.github.com>
2018-12-27 16:06:50 +0700

    Revert "Merge branch 'master' of https://github.com/takashato/vTalk"
    This reverts commit a5838880b9a5689cb93029fcf3c2b0c85c7c23fa, reversing
    changes made to 411c5d64f25b42dc9a86dd298baf9298fc22fa1a.

--- vTalkClient/gui/main/CreateRoomDialog.xaml.cs
+++ vTalkClient/gui/main/CreateRoomDialog.xaml.cs
@@ -27,8 +27,7 @@ namespace vTalkClient.gui.main
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
-            this.Hide();
-            pbPassword.Password = "";
+            this.Close();
         }
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
@@ -39,32 +38,6 @@ namespace vTalkClient.gui.main
             pw.WriteBool(hasPassword);
             if (hasPassword) pw.WriteString(pbPassword.Password);
             ClientWindow.Instance.Client.SendData(SendHeader.CreateRoom, pw.ToArray());
-
-
         }
-
-        #region Process Placehoder "Tên phòng"
-        private void TbName_ProcessFocus(object sender, RoutedEventArgs e)
-        {
-            if (tbName.Text == "Tên phòng")
-            {
-                tbName.Text = "";
-            }
-            else if (tbName.Text == "")
-            {
-                tbName.Text = "Tên phòng";
-                tbName.Foreground = new BrushConverter().ConvertFrom("#666") as SolidColorBrush;
-            }
-        }
-
-        private void TbName_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (tbName.Text != "")
-            {
-                tbName.Foreground = new BrushConverter().ConvertFrom("#000") as SolidColorBrush;
-            }
-        }
-        #endregion
-
     }
 }
