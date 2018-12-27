8bbee95abbc4df730bb4facb15cb01c0835edecb
Thanh Sơn Bành <takashato@gmail.com>
2018-12-27 21:54:19 +0700

    Add manifest and icon for server

--- vTalkServer/Properties/AssemblyInfo.cs
+++ vTalkServer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("vTalkServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ứng dụng server cho vTalk")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("LCS Team UIT")]
 [assembly: AssemblyProduct("vTalkServer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © LCS Team UIT  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
